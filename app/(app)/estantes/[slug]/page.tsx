9f313f27b3158f0900aa1d6d3a50b57b404adb8d
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2026-02-18 01:54:19 -0300

    🌈 style(PaginaEstante): Corrige layout

--- app/(app)/estantes/[slug]/page.tsx
+++ app/(app)/estantes/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { BotaoVoltar } from '@/components/BotaoVoltar';
 import { Api, Estante } from '@/lib/api';
 import { getUsuario } from '@/lib/dal';
 import { getNicho } from '@/lib/utils';
-import { Group, Title } from '@mantine/core';
+import { Container, Group, Title } from '@mantine/core';
 import { AcoesEstante } from '../components/AcoesEstante';
 import { AcoesNicho } from '../components/AcoesNicho';
 import { MembrosEstante } from '../components/MembrosEstante';
@@ -18,8 +18,11 @@ export default async function PaginaEstante({
   const { slug } = await params;
   const usuario = await getUsuario();
   const estante: Estante | undefined = await Api.estantes.buscarPorId(slug);
-  const dono = estante?.usuarios.some((u) => u.id === usuario?.id && u.cargo.toUpperCase() === "DONO") ?? false;
-  
+  const dono =
+    estante?.usuarios.some(
+      (u) => u.id === usuario?.id && u.cargo.toUpperCase() === 'DONO',
+    ) ?? false;
+
   if (!estante) {
     return null;
   }
@@ -35,30 +38,36 @@ export default async function PaginaEstante({
         nicho: getNicho(livro.linha, livro.coluna),
       }))}
     >
-      <BotaoVoltar href="/estantes" />
+      <Container>
+        <BotaoVoltar href="/estantes" />
 
-      <Group gap={8}>
-        <Title order={1} className="text-2xl" mb={8}>
-          {estante.nome}
+        <Group gap={8}>
+          <Title order={1} className="text-2xl" mb={8}>
+            {estante.nome}
+          </Title>
+          <MembrosEstante
+            usuarios={{
+              donos: estante.usuarios.filter(
+                (u) => u.cargo.toUpperCase() === 'DONO',
+              ),
+              membros: estante.usuarios.filter(
+                (u) => u.cargo.toUpperCase() === 'MEMBRO',
+              ),
+            }}
+          />
+          {dono && <AcoesEstante estante={estante} />}
+        </Group>
+
+        <GridEstante estante={estante} dono={dono} />
+
+        {dono && <AcoesNicho estanteId={estante.id} />}
+
+        <Title order={2} className="text-lg" mt={16} mb={8}>
+          Livros na estante
         </Title>
-        <MembrosEstante
-          usuarios={{
-            donos: estante.usuarios.filter((u) => u.cargo.toUpperCase() === "DONO"),
-            membros: estante.usuarios.filter((u) => u.cargo.toUpperCase() === "MEMBRO"),
-          }}
-        />
-        {dono && <AcoesEstante estante={estante} />}
-      </Group>
 
-      <GridEstante estante={estante} dono={dono} />
-
-      {dono && <AcoesNicho estanteId={estante.id} />}
-
-      <Title order={2} className="text-lg" mt={16} mb={8}>
-        Livros na estante
-      </Title>
-
-      <ListaEstante />
+        <ListaEstante />
+      </Container>
     </EstanteContextProvider>
   );
 }
